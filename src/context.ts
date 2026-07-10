1ea07a2de64f396a1fc38b5394c01923c8690eb9
Axodouble <git@axodouble.com>
2026-07-10 01:02:19 +0000

    feat: add ChattoContext with eager, cached message hydration

--- src/context.ts
+++ src/context.ts
@@ -1,12 +1,50 @@
 import type { RestClient } from './rest/client'
 import type { MessageData } from './types'
-import type { Message } from './resources/message'
-import type { User } from './resources/user'
-import type { Room } from './resources/room'
+import { Message } from './resources/message'
+import { User } from './resources/user'
+import { Room } from './resources/room'
+import { UserManager } from './managers/users'
+import { RoomManager } from './managers/rooms'
+import { MessageManager } from './managers/messages'
+import { UserCache, RoomCache } from './caches'
 
 export interface ClientContext {
   readonly rest: RestClient
   resolveUser(id: string): Promise<User>
   resolveRoom(id: string): Promise<Room>
   hydrateMessage(data: MessageData): Promise<Message>
+}
+
+export class ChattoContext implements ClientContext {
+  readonly rest: RestClient
+  readonly users: UserManager
+  readonly rooms: RoomManager
+  readonly messages: MessageManager
+  private readonly userCache: UserCache
+  private readonly roomCache: RoomCache
+
+  constructor(rest: RestClient) {
+    this.rest = rest
+    this.users = new UserManager(this)
+    this.rooms = new RoomManager(this)
+    this.messages = new MessageManager(this)
+    this.userCache = new UserCache(id => this.users.fetch(id))
+    this.roomCache = new RoomCache(id => this.rooms.fetch(id))
+  }
+
+  resolveUser(id: string): Promise<User> {
+    return this.userCache.resolve(id)
+  }
+
+  resolveRoom(id: string): Promise<Room> {
+    return this.roomCache.resolve(id)
+  }
+
+  async hydrateMessage(data: MessageData): Promise<Message> {
+    const [author, channel] = await Promise.all([
+      this.resolveUser(data.actorId).catch(() => User.partial(data.actorId)),
+      this.resolveRoom(data.roomId).catch(() => Room.partial(data.roomId, this)),
+    ])
+    return new Message(data, this, { author, channel })
+  }
 }
